3f061e9479fa45d13b481a7e3af980dc480d9a71
Luca Wilke <l.wilke@uni-luebeck.de>
2023-01-05 10:52:53 +0100

    refactor: annotated timers with #cfg[(target_arch = ...)]

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,56 @@ pub mod rank_bank;
 use anyhow::{bail, Context, Result};
 
 use clap::arg;
-use core::arch::x86_64;
 use core::ptr;
 use std::arch::asm;
 use std::string::String;
 use std::{sync, thread, time};
+
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64;
+
+pub trait MemoryTimer {
+    unsafe fn time_access(&self, a: *const u8) -> u64;
+    unsafe fn flush(&self, a: *const u8);
+}
+
+#[cfg(target_arch = "x86_64")]
+pub struct DefaultMemoryTimer {}
+
+#[cfg(target_arch = "x86_64")]
+impl MemoryTimer for DefaultMemoryTimer {
+    unsafe fn time_access(&self, a: *const u8) -> u64 {
+        let mut timing = 0;
+        asm!(
+            "mfence",
+            "rdtsc",      /*writes to edx:eax*/
+            "shl rdx, 32", /*shift low 32 bits in edx to high bits*/
+            "or rdx,rax",  /*add low bits stored in eax*/
+            "mov rcx, rdx", /*stash measurement in rcx*/
+            "mov rax,  [{a}]",
+            "lfence",
+            "rdtsc",
+            "shl rdx, 32", /*shift low 32 bits in edx to high bits*/
+            "or rdx,rax",  /*add low bits stored in eax*/
+            "sub rdx, rcx", /*calculdate diff*/
+            "mov {timing}, rdx",
+            a = in(reg) a as u64,
+            timing = inout(reg) timing,
+            out("rdx") _, /*mark rdx as clobbered*/
+            out("rax") _, /*mark rax as clobbered*/
+            out("rcx") _, /*mark rcx as clobbered*/
+
+        );
+        return timing;
+    }
+
+    unsafe fn flush(&self, a: *const u8) {
+        asm!(
+            "clflush [{a}]",
+            a = in(reg) a as u64,
+        );
+    }
+}
 
 pub fn construct_timer_from_cli_arg(arg_string: &str) -> Result<Box<dyn MemoryTupleTimer>> {
     let arg_string = String::from(arg_string);
@@ -17,8 +62,10 @@ pub fn construct_timer_from_cli_arg(arg_string: &str) -> Result<Box<dyn MemoryTu
         bail!("unknown timer");
     }
 
+    #[cfg(target_arch = "x86_64")]
     match tokens[0] {
         "rdtsc" => Ok(Box::new(DefaultMemoryTupleTimer {})),
+        "rdtsc-asm" => Ok(Box::new(AsmMemoryTupleTimer {})),
         "counting_thread" => {
             if tokens.len() != 3 {
                 bail!("counting_thread usage: counting_thread,<main cpu>,<counting thread cpu>")
@@ -35,7 +82,12 @@ pub fn construct_timer_from_cli_arg(arg_string: &str) -> Result<Box<dyn MemoryTu
                 counting_thread_cpu,
             )))
         }
-        _ => bail!("unknown timer"),
+        _ => bail!("unknown timer on x86_64 target"),
+    }
+
+    #[cfg(target_arch = "aarch64")]
+    match tokens[0] {
+        _ => bail!("unknown timer on aarch64 target "),
     }
 }
 
@@ -48,8 +100,10 @@ pub trait MemoryTupleTimer {
     ) -> u64;
 }
 
+#[cfg(target_arch = "x86_64")]
 pub struct DefaultMemoryTupleTimer {}
 
+#[cfg(target_arch = "x86_64")]
 impl MemoryTupleTimer for DefaultMemoryTupleTimer {
     ///time_subsequent_access_from_ram measures the access time when accessing both memory locations back to back from ram.
     /// #Arguments
@@ -85,8 +139,10 @@ impl MemoryTupleTimer for DefaultMemoryTupleTimer {
     }
 }
 
+#[cfg(target_arch = "x86_64")]
 pub struct AsmMemoryTupleTimer {}
 
+#[cfg(target_arch = "x86_64")]
 impl MemoryTupleTimer for AsmMemoryTupleTimer {
     unsafe fn time_subsequent_access_from_ram(
         &self,
@@ -128,18 +184,20 @@ impl MemoryTupleTimer for AsmMemoryTupleTimer {
 
 //see https://users.rust-lang.org/t/how-to-have-shared-mutable-state-without-lock/3936
 //for data structure "explanation"
-
+#[cfg(target_arch = "x86_64")]
 struct RacyCounter {
     counter: std::cell::UnsafeCell<usize>,
 }
-
+#[cfg(target_arch = "x86_64")]
 unsafe impl Sync for RacyCounter {}
 
+#[cfg(target_arch = "x86_64")]
 pub struct CountingThreadTupleTimer {
     counter: sync::Arc<RacyCounter>,
     thread_should_terminate: sync::Arc<sync::Mutex<bool>>,
 }
 
+#[cfg(target_arch = "x86_64")]
 impl CountingThreadTupleTimer {
     ///Creates a counting thread based timer. Make sure to place the calling thread and
     /// the counting thread on cores that share at least a L2 cache.
@@ -194,14 +252,14 @@ impl CountingThreadTupleTimer {
         }
     }
 }
-
+#[cfg(target_arch = "x86_64")]
 impl Drop for CountingThreadTupleTimer {
     fn drop(&mut self) {
         let mut term = self.thread_should_terminate.lock().unwrap();
         *term = true;
     }
 }
-
+#[cfg(target_arch = "x86_64")]
 impl MemoryTupleTimer for CountingThreadTupleTimer {
     unsafe fn time_subsequent_access_from_ram(
         &self,
